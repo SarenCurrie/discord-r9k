c1f1cdd55ee526bb0444ab775898672cb1b5608e
Saren Currie <saren@sarencurrie.com>
2017-10-01 12:52:40 +1300

    Refactor app into plugins
    Each individual function is now in a seperate plugin. Each plugin can be
    optionally included in index.js. Database access has been moved to
    seperate files per plugin inside the data directory.

---
+++ src/app.js
@@ -0,0 +1,172 @@
+const Discord = require('discord.io');
+const schedule = require('node-schedule').scheduleJob;
+const pkg = require('../package.json');
+
+if (!process.env.DISCORD_TOKEN) {
+  console.error('DISCORD_TOKEN not set!');
+  process.exit(1);
+}
+
+const bot = new Discord.Client({
+  token: process.env.DISCORD_TOKEN,
+  autorun: true,
+});
+
+const triggers = [];
+const interceptors = [];
+
+let initialized = false;
+
+const addMessageTrigger = (trigger, action) => {
+  if (trigger instanceof Array) {
+    for (let i = 0; i < trigger.length; i += 1) {
+      triggers.push({ trigger: trigger[i], action });
+    }
+  } else {
+    triggers.push({ trigger, action });
+  }
+};
+
+const addRawInterceptor = (eventTypes, ignoreTriggers, action) => {
+  interceptors.push({
+    eventTypes,
+    action,
+    ignoreTriggers,
+  });
+};
+
+const addCronTrigger = (cron, callback) => {
+  schedule(cron, () => {
+    callback(bot);
+  });
+};
+
+const addPlugin = (plugin) => {
+  if (!initialized) {
+    console.console.error('Please call init first.');
+  }
+  plugin.init({
+    addMessageTrigger,  // On submit or edit
+    addCronTrigger, // On scheduled
+    addRawInterceptor, // Intercept everything
+  });
+};
+
+const init = () => new Promise((resolve, reject) => {
+  bot.on('ready', () => {
+    if (initialized) {
+      reject(new Error('Already initialized!'));
+    } else {
+      initialized = true;
+      console.log('Logged in as %s - %s\n', bot.username, bot.id);
+
+      bot.on('disconnect', () => {
+        console.log('Disconnected, attempting to reconnect');
+        bot.connect();
+      });
+
+      const printHelp = () => triggers
+          .filter(trigger => typeof trigger.trigger === 'string')
+          .map(trigger => trigger.trigger)
+          .sort()
+          .reduce((a, b) => `${a}\n${b}`);
+
+      const printVersion = () => `I'm currently running version ${pkg.version}`;
+
+      addMessageTrigger('!help', printHelp);
+      addMessageTrigger('!version', printVersion);
+      addMessageTrigger('!ping', () => 'pong');
+
+      const handleMessage = (user, userId, channelId, message, event) => {
+        let wasTrigger = message.startsWith('!');
+
+        const doTrigger = (trigger, matches) => {
+          console.log('triggered');
+          const result = trigger.action({
+            user,
+            userId,
+            channelId,
+            message,
+            event,
+            bot,
+            matches,
+          });
+
+          console.log('sending this:');
+          console.log(result);
+          console.log(`to channel: ${channelId}`);
+
+          if (result && typeof result === 'string') {
+            bot.sendMessage({
+              to: channelId,
+              message: result,
+            });
+          }
+        };
+
+        for (let i = 0; i < triggers.length; i += 1) {
+          const trigger = triggers[i];
+
+          if (trigger.trigger instanceof RegExp) {
+            const matches = message.match(trigger.trigger);
+            if (matches) {
+              wasTrigger = true;
+              doTrigger(trigger, matches);
+            }
+          } else if (trigger.trigger instanceof Function) {
+            if (trigger.trigger()) {
+              wasTrigger = true;
+              doTrigger(trigger);
+            }
+          } else if (message === trigger.trigger) {
+            wasTrigger = true;
+            doTrigger(trigger);
+          }
+        }
+
+        return wasTrigger;
+      };
+
+      bot.on('any', (event) => {
+        const textEvents = ['MESSAGE_CREATE', 'MESSAGE_UPDATE'];
+        let wasTrigger = false;
+
+        console.log(event);
+
+        if (!event.d) {
+          console.log('Ignoring event with no data.');
+          return;
+        }
+
+        // Ignore other bots
+        if (event.d.author && event.d.author.bot) {
+          console.log('Ignoring bot post.');
+          return;
+        }
+
+        if (event.d.content && textEvents.some(type => type === event.t) && !event.d.pinned) {
+          wasTrigger = handleMessage(event.d.author ? event.d.author.username : null,
+              event.d.author ? event.d.author.id : null,
+              event.d.channel_id,
+              event.d.content,
+              event);
+        }
+
+        interceptors.forEach((interceptor) => {
+          if (wasTrigger && interceptor.ignoreTriggers) {
+            return;
+          }
+          if (interceptor.eventTypes.some(t => t === event.t) && !event.d.pinned) {
+            interceptor.action({ event, bot });
+          }
+        });
+      });
+
+      resolve();
+    }
+  });
+});
+
+exports.init = init;
+exports.addMessageTrigger = addMessageTrigger;
+exports.addPlugin = addPlugin;
---
+++ src/index.js
@@ -0,0 +1,51 @@
+const app = require('./app');
+const statusUtils = require('./status-utils');
+const r9k = require('./plugins/r9k/r9k');
+const markets = require('./plugins/markets/markets');
+const devNull = require('./plugins/dev-null/dev-null');
+
+app.init().then(() => {
+  app.addMessageTrigger('!status', (opts) => {
+    let message = '';
+    statusUtils.getBattery.then((battery) => {
+      // Add battry status to response if available
+      message = message.concat(battery).concat('\n');
+    }).catch(() => {}) // Ignore errors
+    .then(() => {
+      // Add uptime information to response, whether battery check was successful or not, then send message
+      message = message.concat(statusUtils.getUptime());
+      opts.bot.sendMessage({
+        to: opts.channelId,
+        message,
+      });
+    });
+  });
+  app.addMessageTrigger('!battery', (opts) => {
+    statusUtils.getBattery().then((battery) => {
+      opts.bot.sendMessage({
+        to: opts.channelId,
+        message: battery,
+      });
+    }).catch(() => {
+      opts.bot.sendMessage({
+        to: opts.channelId,
+        message: 'Error getting battery data.',
+      });
+    });
+  });
+
+  app.addMessageTrigger('!uptime', statusUtils.getUptime);
+  app.addPlugin(r9k);
+  app.addPlugin(markets);
+  app.addPlugin(devNull({ channel: '349421970083020801' }));
+
+  try {
+    app.addPlugin(require('./secret-triggers.js')); // eslint-disable-line global-require, no-unresolved
+  } catch (e) {
+    console.warn('secret-triggers.js does not exist or is broken, ignoring.');
+  }
+}).catch((e) => {
+  console.error('Error starting up');
+  console.error(e);
+  process.exit(1);
+});
